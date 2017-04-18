c777174b2f3303cb68f1211e027fa16a41cf5baa
Gustavo Bezerra <gusutabopb@gmail.com>
2017-04-18 18:13:35 +0900

    Update unit test

--- tests/test_corintick.py
+++ tests/test_corintick.py
@@ -1,35 +1,33 @@
 import pandas as pd
 import pytest
 
-from corintick import Writer, Reader
+from corintick import Corintick
 
-@pytest.fixture
-def writer():
-    return Writer(dbname='corintick_test')
-
-
-@pytest.fixture
-def reader():
-    return Reader(dbname='corintick_test')
+@pytest.fixture(scope="module")
+def api():
+    api = Corintick(config='~/plugaai/corintick/config.yml')
+    print(api.config)
+    return api
 
 
-def test_simple_write(writer):
+def test_simple_write(api):
     import pandas_datareader as pdr
     import quandl
     uid1 = 'AAPL'
     uid2 = '7203'
     df1 = pdr.get_data_yahoo(uid1)
     df2 = quandl.get('TSE/{}'.format(uid2))
-    r1 = writer.write(uid1, df1, source='Yahoo Finance')
-    r2 = writer.write(uid2, df2, source='Quandl')
+    print(df1.head())
+    print(df2.head())
+    r1 = api.write(uid1, df1, source='Yahoo Finance')
+    r2 = api.write(uid2, df2, source='Quandl')
     assert r1.acknowledged
     assert r2.acknowledged
 
 
-def test_simple_read(reader):
-    yahoo = reader.read('AAPL')
-    quandl = reader.read('7203')
-    assert len(yahoo) > 100
-    assert len(quandl) > 100
-    assert isinstance(yahoo, pd.DataFrame)
-    assert isinstance(yahoo, pd.DataFrame)
+def test_simple_read(api):
+    for df in [api.read('AAPL'), api.read('7203')]:
+        assert isinstance(df, pd.DataFrame)
+        print(df.head(), df.shape)
+        assert len(df) > 100
+
